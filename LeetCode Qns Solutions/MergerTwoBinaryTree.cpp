db6537898164df0aa1fbb89508f2aebc4774c9cf
PADMALOCHAN PANDA <pl5141panda@gmail.com>
2021-08-05 20:23:40 +0530

    Create MergerTwoBinaryTree.cpp

---
+++ LeetCode Qns Solutions/MergerTwoBinaryTree.cpp
@@ -0,0 +1,25 @@
+//https://leetcode.com/problems/merge-two-binary-trees/submissions/
+#include<stdio.h>
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
+        if(root1 == NULL && root2 == NULL)
+            return NULL;
+        if(root1 == NULL && root2 != NULL)
+            return root2;
+        if(root1 != NULL && root2 == NULL)
+            return root1;
+        root1->val += root2->val;
+        root1->left = mergeTrees(root1->left, root2->left);
+        root1->right = mergeTrees(root1->right, root2->right);
+        return root1;
+    }
+};
