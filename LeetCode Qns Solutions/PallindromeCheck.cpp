79e679ab4a4ffd474b09f457f1e21a1ff8aad461
PADMALOCHAN PANDA <pl5141panda@gmail.com>
2023-09-28 22:38:51 +0530

    Create PallindromeCheck.cpp

---
+++ LeetCode Qns Solutions/PallindromeCheck.cpp
@@ -0,0 +1,15 @@
+#https://leetcode.com/problems/palindrome-number/
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x < 0 || (x != 0 && x % 10 == 0)) return false;
+        if(x < 10) return true;
+        long y = 0;
+        int z = x;
+        while(x > 0){
+            y = y*10 + (x%10);
+            x = x/10;
+        }
+        return (y == z)?true:false;
+    }
+};
