1861356e99d0cefa85db9af4a7e39a2ad63b535b
PADMALOCHAN PANDA <pl5141panda@gmail.com>
2023-10-06 06:39:46 +0530

    Update MajorityElement2.cpp

--- LeetCode Qns Solutions/MajorityElement2.cpp
+++ LeetCode Qns Solutions/MajorityElement2.cpp
@@ -12,4 +12,36 @@ public:
                 a.push_back(i.first);        
         return a;
     }
+	vector<int> majorityElement2(vector<int>& nums) {
+        int c1 = 0, c2 = 0, n1, n2;
+        int l = nums.size()/3;
+        for(auto a:nums){
+            if(c1 == 0 && n2 != a){
+                n1 = a;
+                c1++;
+            }else if(c2 == 0 && n1 != a){
+                n2 = a;
+                c2++;
+            }else if(n1 == a){
+                c1++;
+            }else if(n2 == a){
+                c2++;
+            }else{
+                c1--;
+                c2--;
+            }
+        }
+        c1 = 0, c2 = 0;
+        for(auto a:nums)
+            if(n1 == a)
+                c1++;
+            else if(n2 == a)
+                c2++;
+        vector<int> a;   
+        if(c1 > l)
+            a.push_back(n1);
+        if(c2 > l)
+            a.push_back(n2);     
+        return a;
+    }
 };
