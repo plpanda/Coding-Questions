5b29a7cf6daf1a77bd002ef74b83e859521216b3
PADMALOCHAN PANDA <pl5141panda@gmail.com>
2023-11-11 19:35:22 +0530

    Update ProductOfArray.cpp

--- LeetCode Qns Solutions/ProductOfArray.cpp
+++ LeetCode Qns Solutions/ProductOfArray.cpp
@@ -1,7 +1,19 @@
 //https://leetcode.com/problems/product-of-array-except-self/
 class Solution {
 public:
-    vector<int> productExceptSelf(vector<int>& nums) {
+	vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> ret;
+        int n = nums.size(), rProd = 1;
+        ret.push_back(1);
+        for(int i = 0; i < n-1; ++i)
+            ret.push_back(ret[i] * nums[i]);
+        for(int i = n-1; i >= 0; --i){
+            ret[i] *= rProd;
+            rProd *= nums[i];
+        }
+        return ret;
+    }
+    vector<int> productExceptSelf2(vector<int>& nums) {
         vector<int> p1, p2, ret;
         int n = nums.size();
         p1.push_back(nums[0]);
