2803f840f1b18e6ad2f579c05cddc3261cd19c26
PADMALOCHAN PANDA <pl5141panda@gmail.com>
2023-11-24 20:08:01 +0530

    Create CoinChange.cpp

---
+++ LeetCode Qns Solutions/CoinChange.cpp
@@ -0,0 +1,16 @@
+//https://leetcode.com/problems/coin-change/
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount+1, INT_MAX);
+        dp[0] = 0;
+        for(int i = 1; i <= amount; ++i){
+            for(int coin : coins)
+                if(coin <= i && dp[i-coin] != INT_MAX)
+                    dp[i] = min(dp[i], dp[i-coin] + 1);
+        }
+        if(dp[amount] == INT_MAX)
+            return -1;
+        return dp[amount];
+    }
+};
